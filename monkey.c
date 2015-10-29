78aee55c8dc58bffe77d1a9d0b8f32c45cb633c0
Andrew Groot <groot.andrew@gmail.com>
2015-10-28 23:34:22 -0400

    Minimal output by default.

--- monkey.c
+++ monkey.c
@@ -10,14 +10,16 @@ const size_t DEFAULT_READ_MAX = 1024;
 const char * DEFAULT_SOURCE_FILE = "_test.c";
 const char * DEFAULT_OUTPUT_FILE = "_a.out";
 
-const size_t MAX_COMMAND_SIZE = 128;
-const char * COMPILE_COMMAND_FORMAT = "gcc -o \"%s\" \"%s\" > /dev/null";
+const size_t MAX_COMMAND_SIZE = 256;
+const char * COMPILE_COMMAND_FORMAT = "gcc -o \"%s\" \"%s\"";
+const char * COMPILE_COMMAND_ERROR_REDIRECT = "&> /dev/null";
 
-bool tryNextRandomProgram(size_t read_max, const char * source_file, const char * output_file);
+bool tryNextRandomProgram(const bool verbose, size_t read_max, const char * source_file, const char * output_file);
 char * getPrintableCharacters(size_t size);
 
 int main(int argc, char * argv[]) {
     // TODO: override with command-line arguments
+    const bool verbose = 0;
     const size_t read_max = DEFAULT_READ_MAX;
     const char * output_file = DEFAULT_OUTPUT_FILE;
     const char * source_file = DEFAULT_SOURCE_FILE;
@@ -29,6 +31,10 @@ int main(int argc, char * argv[]) {
 
     char compile_command[MAX_COMMAND_SIZE];
     int command_length = snprintf(compile_command, MAX_COMMAND_SIZE, COMPILE_COMMAND_FORMAT, output_file, source_file);
+    if (!verbose) {
+        strncat(compile_command, COMPILE_COMMAND_ERROR_REDIRECT, MAX_COMMAND_SIZE);
+        command_length += strlen(COMPILE_COMMAND_ERROR_REDIRECT);
+    }
     if (command_length < 0 || command_length >= MAX_COMMAND_SIZE) {
         printf("Unable to built command.  Built [%s].", compile_command);
         return 1;
@@ -36,24 +42,30 @@ int main(int argc, char * argv[]) {
 
     unsigned int attempt = 1;
     while (1) {
-        printf("Attempt %d.\n", attempt);
-        bool success = tryNextRandomProgram(read_max, compile_command, source_file);
+        if (verbose) {
+            printf("Attempt %d.\n", attempt);
+        }
+        bool success = tryNextRandomProgram(verbose, read_max, compile_command, source_file);
         if (success) {
             printf("Success!  See %s for the valid program!\n", source_file);
             printf("  WARNING: You probably don't want to run %s!\n", output_file);
             break;
         } else {
-            printf("Failed.\n");
-            printf("--------------------\n");
+            if (verbose) {
+                printf("Failed.\n");
+                printf("--------------------\n");
+            }
             attempt++;
         }
     }
 }
 
-bool tryNextRandomProgram(const size_t read_max, const char * compile_command, const char * source_file) {
+bool tryNextRandomProgram(const bool verbose, const size_t read_max, const char * compile_command, const char * source_file) {
     const size_t size = rand() % read_max;
 
-    printf("Reading %d printable characters.\n", size);
+    if (verbose) {
+        printf("Reading %d printable characters.\n", size);
+    }
     const char * text = getPrintableCharacters(size);
 
     FILE * f = fopen(source_file, "w");
