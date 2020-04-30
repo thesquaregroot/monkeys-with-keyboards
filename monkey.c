93b52cb689539778f29267b579cbb2e171033290
Andrew Groot <groot.andrew@gmail.com>
2020-04-29 23:55:00 -0400

    Various improvements output and verbose/silent options.

--- monkey.c
+++ monkey.c
@@ -12,11 +12,12 @@ const char * DEFAULT_SOURCE_FILE = "_test.c";
 const char * DEFAULT_OUTPUT_FILE = "_a.out";
 
 const size_t MAX_COMMAND_SIZE = 256;
-const char * COMPILE_COMMAND_FORMAT = "gcc -Wfatal-errors -o \"%s\" \"%s\"";
-const char * COMPILE_COMMAND_ERROR_REDIRECT = "&> /dev/null";
+const char * COMPILE_COMMAND_FORMAT = "/usr/bin/gcc -Wfatal-errors -o \"%s\" \"%s\"";
+const char * COMPILE_COMMAND_ERROR_REDIRECT = " > /dev/null 2>&1";
 
-bool buildCommand(char * compile_command, bool verbose, const char * output_file, const char * source_file);
-bool tryNextRandomProgram(size_t size, const char * compile_command, const char * output_file);
+bool doesOutputFileExist(const char * output_file);
+bool buildCommand(char * compile_command, bool verbose, const char * source_file, const char * output_file);
+bool tryNextRandomProgram(size_t size, const char * compile_command, const char * source_file, const char * output_file);
 char * getPrintableCharacters(size_t size);
 
 int main(int argc, char * argv[]) {
@@ -27,10 +28,18 @@ int main(int argc, char * argv[]) {
     const char * source_file = DEFAULT_SOURCE_FILE;
 
     int c;
-    while ((c = getopt (argc, argv, "hi:n:o:sv")) != -1) {
+    while ((c = getopt(argc, argv, "hi:n:o:sv")) != -1) {
         switch (c) {
         case 'h':
             printf("Usage: %s [-h] [-v] [-s] [-n max_size] [-i source_file] [-o output_file]\n", argv[0]);
+            printf("\n");
+            printf(" -h         Display this help content\n");
+            printf(" -v         Enable verbose mode\n");
+            printf(" -s         Enable silent mode\n");
+            printf(" -n [N]     Set the maximum number of characters to add to the input file\n");
+            printf(" -i [file]  Override the default source file name of %s\n", DEFAULT_SOURCE_FILE);
+            printf(" -o [file]  Override the default output file name of %s\n", DEFAULT_OUTPUT_FILE);
+            printf("\n");
             return 0;
         case 'i':
             source_file = optarg;
@@ -67,24 +76,30 @@ int main(int argc, char * argv[]) {
     time_t now;
     srand((unsigned) time(&now));
 
-    setbuf(stdout, NULL); // disable stdout buffering
-
     char compile_command[MAX_COMMAND_SIZE];
-    bool good_command = buildCommand(compile_command, verbose, output_file, source_file);
+    bool good_command = buildCommand(compile_command, verbose, source_file, output_file);
     if (!good_command) {
         printf("Unable to built command.  Built [%s].", compile_command);
         return 1;
+    }
+
+    if (doesOutputFileExist(output_file)) {
+        int result = remove(output_file);
+        if (result != 0) {
+            printf("Unable to delete existing output file: %s", output_file);
+            return 1;
+        }
     }
 
     unsigned long attempt = 1;
     while (1) {
         const size_t size = rand() % (read_max+1);
         if (!silent) {
-            printf("Attempt %d (%d characters).\n", attempt, size);
+            printf("Attempt %ld (%ld characters).\n", attempt, size);
         }
-        bool success = tryNextRandomProgram(size, compile_command, source_file);
+        bool success = tryNextRandomProgram(size, compile_command, source_file, output_file);
         if (success) {
-            printf("Success after %d attempts!  See %s for the valid program!\n", attempt, source_file);
+            printf("Success after %ld attempts!  See %s for the valid program!\n", attempt, source_file);
             printf("  WARNING: You probably don't want to run %s!\n", output_file);
             break;
         } else {
@@ -97,16 +112,16 @@ int main(int argc, char * argv[]) {
     }
 }
 
-bool buildCommand(char * compile_command, bool verbose, const char * output_file, const char * source_file) {
+bool buildCommand(char * compile_command, bool verbose, const char * source_file, const char * output_file) {
     int command_length = snprintf(compile_command, MAX_COMMAND_SIZE, COMPILE_COMMAND_FORMAT, output_file, source_file);
     if (!verbose) {
-        strncat(compile_command, COMPILE_COMMAND_ERROR_REDIRECT, MAX_COMMAND_SIZE);
+        strncat(compile_command, COMPILE_COMMAND_ERROR_REDIRECT, MAX_COMMAND_SIZE - command_length);
         command_length += strlen(COMPILE_COMMAND_ERROR_REDIRECT);
     }
     return command_length > 0 && command_length < MAX_COMMAND_SIZE;
 }
 
-bool tryNextRandomProgram(size_t size, const char * compile_command, const char * source_file) {
+bool tryNextRandomProgram(size_t size, const char * compile_command, const char * source_file, const char * output_file) {
     const char * text = getPrintableCharacters(size);
 
     FILE * f = fopen(source_file, "w");
@@ -114,7 +129,9 @@ bool tryNextRandomProgram(size_t size, const char * compile_command, const char 
     fclose(f);
 
     int ret = system(compile_command);
-    return ret == 0;
+
+    int executable_created = doesOutputFileExist(output_file);
+    return (ret == 0) && executable_created;
 }
 
 char getRandomCharacter() {
@@ -133,5 +150,9 @@ char * getPrintableCharacters(size_t size) {
     }
     text[size] = '\0'; // null-terminate
     return text;
+}
+
+bool doesOutputFileExist(const char * output_file) {
+    return access(output_file, F_OK) != -1;
 }
 
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./monkey &> out.log
+./monkey $@ &> out.log
 
